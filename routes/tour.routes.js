b8bd40c62f8468f8f7a947230283fc03b043e6a0
Md Najmul Haque <najmulhaque008@gmail.com>
2022-10-05 13:20:38 +0600

    update a tour by ID route is created

--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -7,6 +7,6 @@ const viewCount = require("../middleware/viewCount.js");
 const tourRoutes = express.Router()
 
 tourRoutes.route('/tours').get(tourController.getAllTour).post(tourController.createTour)
-tourRoutes.route('/tour/:id').get(viewCount, tourController.getTourById)
+tourRoutes.route('/tour/:id').get(viewCount, tourController.getTourById).patch(tourController.updateTour)
 
 module.exports=tourRoutes; 
